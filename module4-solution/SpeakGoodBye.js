48a2db0f2560bae4a9c3d877999681e0253de45a
skyByt <fredjkharroubi@gmail.com>
2022-05-21 01:07:37 -0400

    JS module 4 solution

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function () {
+  var speakWord = "Good Bye";
+  var byeSpeaker = {};
+  byeSpeaker.speak= function (name) {
+    console.log(speakWord + " " + name);
+  }
+  window.byeSpeaker = byeSpeaker;
+})(window);
